bbb4593107fa0d0f494034e105acd910ba3dba83
BuildTools <unconfigured@null.spigotmc.org>
2020-10-08 07:43:31 +0800

    Add project files.

---
+++ LabExer2MacapundagJohari/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExer2MacapundagJohari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                Console.Write("x = ");
+                string str = Console.ReadLine();
+                if(str == "exit")
+                {
+                    Console.WriteLine("Program has stopped");
+                    break;
+                }
+                else
+                {
+                    int x = Convert.ToInt32(str);
+                    Console.Write("y = ");
+                    int y = Convert.ToInt32(Console.ReadLine());
+                    int sum = x + y;
+                    Console.WriteLine("Result: {0}", sum);
+                }
+
+            } while (true);
+
+            
+        }
+    }
+}
